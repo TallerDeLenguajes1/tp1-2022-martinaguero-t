51ae5ec974f121948e687c22d29b9e0e6aaf913e
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-08-26 18:24:00 -0300

    Problema 1

---
+++ 2-PROBLEMAS/Problema1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problema1;
+class Program {
+
+    static void Main(string[] args){
+
+        Console.WriteLine("2) PROBLEMA 1:");
+        Console.Write("Ingrese un número entero: ");
+        
+        int numero;
+
+        Int32.TryParse(Console.ReadLine(), out numero);
+
+        Console.WriteLine($"El cuadrado del número es: {calcularCuadrado(numero)}");
+
+        Console.ReadLine();
+    }
+
+    static int calcularCuadrado(int n){
+        return n*n;
+    }
+
+
+}
