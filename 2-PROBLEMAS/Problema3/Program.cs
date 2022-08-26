7213f5cb964dbc10108d9a710ea7b5a683dd7548
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-08-26 19:14:42 -0300

    Problema 3

--- 2-PROBLEMAS/Problema3/Program.cs
+++ 2-PROBLEMAS/Problema3/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Problema2;
+
+class Program {
+
+    static void Main(string[] args){
+
+        Console.WriteLine("2) PROBLEMA 3:");
+
+        uint cantKilometros, cantLitros;
+
+
+        try 
+        {
+            Console.Write("Ingrese la cantidad de kilómetros recorridos (número entero): ");
+            cantKilometros = Convert.ToUInt32(Console.ReadLine());
+
+            Console.Write("Ingrese la cantidad de litros consumidos (número entero): ");
+            cantLitros = Convert.ToUInt32(Console.ReadLine());
+
+            Console.WriteLine($"Kilómetros por litro: {cantKilometros/cantLitros}");
+            
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: ingrese un número válido (error de formato)");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: ¡la cantidad de litros consumidos no puede ser cero!");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: no se puede trabajar con números de gran magnitud.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+        
+        Console.ReadLine();
+    }
+
+
+}
