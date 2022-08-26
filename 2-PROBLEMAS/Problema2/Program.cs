d239e6a657ddb83fa67929334ee1a1ddfe3ad0f9
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-08-26 18:57:12 -0300

    Corrección problema 2

--- 2-PROBLEMAS/Problema2/Program.cs
+++ 2-PROBLEMAS/Problema2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Problema2;
+class Program {
+
+    static void Main(string[] args){
+
+        Console.WriteLine("2) PROBLEMA 2:");
+        Console.WriteLine("Se calculará la división entre dos números enteros: ");
+        
+        int dividendo, divisor;
+
+        try
+        {
+            Console.Write("Ingrese el dividendo: ");
+            dividendo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el divisor: ");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"El cociente es: {dividendo/divisor}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: ¡no se puede dividir en cero!");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("¡Error de formato (ingrese un número válido)!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error encontrado: {e.Message}");
+        }
+
+        Console.ReadLine();
+    }
+
+
+
+}
